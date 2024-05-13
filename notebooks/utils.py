9b44e585a289ee8d803396dd1cfa343b77130a6d
Mahmudur Rahman <raianmr@gmail.com>
2024-05-14 03:49:15 +0600

    add evaluation

---
+++ notebooks/utils.py
@@ -0,0 +1,14 @@
+import gensim
+
+def preprocess_text(text: str) -> str:
+    text = text.replace('\r', ' ')
+    text = text.replace('\n', ' ')
+    text = gensim.parsing.preprocessing.strip_multiple_whitespaces(text)
+
+    return text
+
+labelnum = {
+    'negative': 0,
+    'neutral': 1,
+    'positive': 2
+}
