6b47033518ef6cefbaa02974277d5dc0e2a61560
Mahmudur Rahman <raianmr@gmail.com>
2024-05-14 07:34:29 +0600

    add inference notebook

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,6 +1,9 @@
 import gensim
 
 def preprocess_text(text: str) -> str:
+    if text is None:
+        return ' '
+
     text = text.replace('\r', ' ')
     text = text.replace('\n', ' ')
     text = gensim.parsing.preprocessing.strip_multiple_whitespaces(text)
