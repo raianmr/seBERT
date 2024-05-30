5e342765a7d7b5fc4137b8ed09bbc1b630de84a6
Mahmudur Rahman <raianmr@gmail.com>
2024-05-31 02:29:41 +0600

    add issue comments

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,7 +1,7 @@
 import gensim
 
 def preprocess_text(text: str) -> str:
-    if text is None:
+    if not isinstance(text, str):
         return ' '
 
     text = text.replace('\r', ' ')
@@ -15,3 +15,23 @@ labelnum = {
     'neutral': 1,
     'positive': 2
 }
+
+def feed(text):
+    from transformers import BertTokenizer, BertForSequenceClassification, pipeline
+
+    MODEL_PATH = './models/sentiment/'
+
+    model = BertForSequenceClassification.from_pretrained(MODEL_PATH)
+    tokenizer = BertTokenizer.from_pretrained(MODEL_PATH)
+
+    livetest = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer, truncation=True, max_length=512)
+
+    result = livetest(text)
+    print(result)
+    label_map = {'LABEL_0': 'negative',
+                 'LABEL_1': 'neutral',
+                 'LABEL_2': 'positive'}
+    answer = {label_map[x['label']]: round(x['score'], 4) for x in result}
+    print(text)
+    print('\nclassified as:')
+    print(answer)
